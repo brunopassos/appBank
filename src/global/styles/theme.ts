018426370c54bc5bcf3f1957c014ad00beaaa563
Bruno <brunopassos_bp@hotmail.com>
2022-12-24 18:09:49 -0300

    feat: first commit, readme updated, created global style and fonts load

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,43 @@
+export default {
+    colorsHomeScreen: {
+
+        header1: "#CECECE",
+        header2: "#F9F9F9",
+        header3: "#D8D8D8",
+
+        backgroundPrincipal: "#414A61",
+        backgroundSecondary: "#000000",
+
+        navigation1: "#8D8D8D",
+        iconNoSelected: "#EEEEEE",
+
+        functions: "#DBE3F8",
+        iconFunctions: "rgba(3, 25, 82, 0.86)",
+
+        myCards: "#292929",
+        myCardsLine: "#585858",
+        cardIconBackground: "#E9E9E9",
+
+        transactionIconBackground: "#FFDEE2",
+        transactionIcon: "#A32E2E",
+
+
+    },
+    colorsTransactionsScreen: {
+        tabNotSelected: "rgba(65, 74, 97, 0.5)",
+        income: "#439E41",
+        outcome: "#C5626E"
+    },
+    colorsTransferScreen: {
+        accountDataBackground: "#7485B4",
+        paginationSelected: "#B1BEEC",
+        paginationNotSelected: "#6C6C6C",
+        amountLine: "#626262",
+        purpouseIcon: "#929292"
+    },
+    fonts: {
+        regular: "Poppins_400Regular",
+        medium: "Poppins_500Medium",
+        bold: "Poppins_700Bold",
+    }
+}
