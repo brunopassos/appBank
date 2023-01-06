9ea111ace4e5839f9746bdeae80976ffd918bf46
Bruno Passos <brunopassos_bp@hotmail.com>
2023-01-06 16:11:01 -0300

    feat: implemented options on transacions screen

--- src/screens/TransactionScreen/index.tsx
+++ src/screens/TransactionScreen/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { Text } from 'react-native-svg';
+
+import IncomeSvg from "../../assets/income.svg";
+import ExpenseSvg from "../../assets/expense.svg";
 
 import {
  Container,
- Header
+ Header,
+ TransactionsScreenOptions,
+ TitleAndFilter,
+ FilterOptions,
+ Title,
+ FilterButton,
+ Filter,
+ FilterAll,
+ FilterAllText,
+ FilterIncome,
+ FilterIncomeText,
+ FilterExpense,
+ FilterExpenseText
 } from './styles';
 
 export function TransactionScreen(){
@@ -12,6 +26,37 @@ export function TransactionScreen(){
             <Header>
                 Transações
             </Header>
+            <TransactionsScreenOptions>
+                <TitleAndFilter>
+                    <Title>
+                        Recentes
+                    </Title>
+                    <FilterButton>
+                        <Filter>
+                            Selecione o perído
+                        </Filter>
+                    </FilterButton>
+                </TitleAndFilter>
+                <FilterOptions>
+                    <FilterAll>
+                        <FilterAllText>
+                            Todas
+                        </FilterAllText>
+                    </FilterAll>
+                    <FilterIncome>
+                        <IncomeSvg width={16} height={16}/>
+                        <FilterIncomeText>
+                            Entradas                            
+                        </FilterIncomeText>
+                    </FilterIncome>
+                    <FilterExpense>
+                        <ExpenseSvg width={16} height={16}/>
+                        <FilterExpenseText>
+                            Gastos
+                        </FilterExpenseText>
+                    </FilterExpense>
+                </FilterOptions>
+            </TransactionsScreenOptions>
         </Container>
     );
 }
